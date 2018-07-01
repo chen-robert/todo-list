105e1b24d3ce6cf061ec0362d8764090d973c992
gamester543 <gamesterrex@gmail.com>
2018-06-30 22:41:19 -0700

    Started components for todolist

---
+++ src/Column.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Column extends Component{
+  render(){
+    return (
+      <div>
+        <p>Column</p>
+      </div>
+    );
+  }
+}
+
+export default Column;
