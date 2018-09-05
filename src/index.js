6237000ecf86da4fafd1fac9d6dafc28019478cf
Robert Chen <robertchen@live.com>
2018-09-04 21:16:39 -0700

    Removed service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom";
 import "./css/index.css";
 import "../node_modules/daemonite-material/css/material.min.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
--- src/registerServiceWorker.js
+++
@@ -1,7 +0,0 @@
-const unregister = () => navigator.serviceWorker.getRegistrations().then((registrations) => {
-  for(let registration of registrations) {
-    registration.unregister()
-  }
-});
-
-export default unregister;
